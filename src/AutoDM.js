14ee8f602d206829a41137b7f8014eb874c2284d
ghalib5000 <ghalibahmed5000@gmail.com>
2019-09-09 18:06:47 +0500

    last resort

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -175,10 +175,8 @@ upload_random_image(images);
     var image_path = "./img/"
     console.log("image not found, preparing to download image...");
 
-    random_image.file = "10.jpg";
-    
     console.log("new name is :"+random_image.file);
-    img_downloader(random_image.file)
+    img_downloader("10.jpg");
 
       console.log("image downloaded");
 
