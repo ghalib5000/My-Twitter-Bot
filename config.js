c700d8934002a7d9c1d6c3425f8571b478191650
ghalib5000 <ghalibahmed5000@gmail.com>
2022-02-07 23:57:51 +0500

    fixed actions

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const twitterApp = {
-  consumer_key: env.CONSUMER_KEY,
-  consumer_secret: env.CONSUMER_KEY_SECRET,
-  access_token: env.ACCESS_TOKEN,
-  access_token_secret: env.ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_KEY_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
 };
 console.log(twitterApp.consumer_key)
