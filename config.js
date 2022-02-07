a8a98c40df69abdca46670250615055ac359a96f
ghalib5000 <ghalibahmed5000@gmail.com>
2022-02-07 23:05:14 +0500

    fixed actions file

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const twitterApp = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_KEY_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  consumer_key: secrets.CONSUMER_KEY,
+  consumer_secret: secrets.CONSUMER_KEY_SECRET,
+  access_token: secrets.ACCESS_TOKEN,
+  access_token_secret: secrets.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
 };
 
 module.exports = {
   twitterApp,
-  userName:  process.env.USERNAME
+  userName:  secrets.USERNAME
 };
