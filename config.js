1d5d133001f7931174a6698b6aefeb211768c8d3
ghalib5000 <ghalibahmed5000@gmail.com>
2022-02-07 23:35:01 +0500

    fixed actions

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
+var t = require(".github/workflows/run.yml");
+//import github actions secret
+
 const twitterApp = {
-  consumer_key: CONSUMER_KEY,
-  consumer_secret: CONSUMER_KEY_SECRET,
-  access_token: ACCESS_TOKEN,
-  access_token_secret: ACCESS_TOKEN_SECRET,
+  consumer_key:  t.CONSUMER_KEY,
+  consumer_secret: t.CONSUMER_KEY_SECRET,
+  access_token: t.ACCESS_TOKEN,
+  access_token_secret: t.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
 };
 
 module.exports = {
   twitterApp,
-  userName:  USERNAME
+  userName:  t.USERNAME
 };
